c4040246bbf307bfb2d77ca8be1fc5aa2aa68381
Filip Hracek <filip.hracek@gmail.com>
2017-05-09 16:21:20 -0700

    Code diet
    Makes the app a bit slimmer where appropriate

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -4,24 +4,18 @@ import 'package:angular2/core.dart';
 import 'package:angular2_components/angular2_components.dart';
 import 'package:firebase/firebase.dart';
 
+typedef T UpdateFunction<T>(T value);
+
 @Component(
     selector: 'main-app',
     styleUrls: const ['app_component.css'],
     templateUrl: 'app_component.html',
     directives: const [materialDirectives],
     providers: const [materialProviders])
-class AppComponent {
+class AppComponent implements OnInit {
   DatabaseReference _ref;
-  int count;
 
-  AppComponent() {
-    _ref = database().ref('counter');
-
-    _ref.onValue.listen((e) {
-      DataSnapshot snapshot = e.snapshot;
-      count = snapshot.val();
-    });
-  }
+  int count = 0;
 
   dislike() async {
     count = await _updateDatabase((c) => c - 1);
@@ -29,6 +23,21 @@ class AppComponent {
 
   like() async {
     count = await _updateDatabase((c) => c + 1);
+  }
+
+  @override
+  ngOnInit() {
+    initializeApp(
+        apiKey: "AIzaSyAcd73Ww0ojuMsBpHM58EKtTvxkmsgAG0A",
+        authDomain: "counter-2a392.firebaseapp.com",
+        databaseURL: "https://counter-2a392.firebaseio.com",
+        storageBucket: "counter-2a392.appspot.com");
+
+    _ref = database().ref('counter');
+    _ref.onValue.listen((e) {
+      DataSnapshot snapshot = e.snapshot;
+      count = snapshot.val();
+    });
   }
 
   Future<int> _updateDatabase(UpdateFunction<int> update) async {
@@ -42,5 +51,3 @@ class AppComponent {
     return transaction.snapshot.val();
   }
 }
-
-typedef T UpdateFunction<T>(T value);
--- web/main.dart
+++ web/main.dart
@@ -1,13 +1,6 @@
 import 'package:angular2/platform/browser.dart';
-import 'package:firebase/firebase.dart';
 import 'package:firebase_counter/app_component.dart';
 
 void main() {
-  initializeApp(
-      apiKey: "AIzaSyAcd73Ww0ojuMsBpHM58EKtTvxkmsgAG0A",
-      authDomain: "counter-2a392.firebaseapp.com",
-      databaseURL: "https://counter-2a392.firebaseio.com",
-      storageBucket: "counter-2a392.appspot.com");
-
   bootstrap(AppComponent);
 }
