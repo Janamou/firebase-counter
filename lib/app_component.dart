bad26d9a390725e2ea2e790dfbfc09ae8f9dd717
Filip Hracek <filip.hracek@gmail.com>
2017-05-10 11:48:33 -0700

    Step 8

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,6 +1,8 @@
 // Copyright (c) 2017. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'package:firebase/firebase.dart';
+
 import 'package:angular2/core.dart';
 import 'package:angular_components/angular_components.dart';
 
@@ -16,6 +18,15 @@ import 'todo_list/todo_list_component.dart';
   directives: const [materialDirectives, TodoListComponent],
   providers: const [materialProviders],
 )
-class AppComponent {
+class AppComponent implements OnInit {
   int count = 0;
+
+  @override
+  ngOnInit() {
+    initializeApp(
+        apiKey: "AIzaSyAH7S_gsce9RtNI8w0z7doiP3ugVJM8ZbI",
+        authDomain: "angulardart-firebase-io-2017.firebaseapp.com",
+        databaseURL: "https://angulardart-firebase-io-2017.firebaseio.com",
+        storageBucket: "angulardart-firebase-io-2017.appspot.com");
+  }
 }
